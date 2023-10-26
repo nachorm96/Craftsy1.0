8e4c90cdaad13176863c848eb0747d8cb19dd743
nachorm96 <rubio.nacho@yahoo.com.ar>
2023-10-26 10:37:13 -0300

    craftsy 1.0

--- app.js
+++ app.js
@@ -7,8 +7,5 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
-app.get('/about', (req,res) => res.sendFile(path.join(__dirname, 'views','about.html')))
-app.get('/music', (req,res) => res.sendFile(path.join(__dirname, 'views','music.html')))
-app.get('/contact', (req,res) => res.sendFile(path.join(__dirname, 'views','contact.html')))
 
 app.listen(PORT,() => console.log(`Servidor corriendo en http://localhost:${PORT} `))
